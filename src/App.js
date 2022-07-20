53332f7454886d495ef5ce9636f48c2d3d5bbd3b
afifi <afifi@besquare.com.my>
2022-07-20 23:21:18 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
 
-function App() {
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
+
+import { useState } from "react";
+
+export default function App() {
+  const [number, setNumber] = useState(0);
+  const [hoverStar, setHoverStar] = useState(undefined);
+
+  const handleText = () => {
+    switch (number || hoverStar) {
+      case 0:
+        return "Evaluate";
+      case 1:
+        return "Dissatifation";
+      case 2:
+        return "Unsatisfied";
+      case 3:
+        return "Normal";
+      case 4:
+        return "Satisfied";
+      case 5:
+        return "Very Satisfied";
+      default:
+        return "Evaluate";
+    }
+  };
+
+  const handlePlaceHolder = () => {
+    switch (number || hoverStar) {
+      case 0:
+        return "Comment here...";
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+        return "What need to be improved?";
+      case 5:
+        return "Why do you like the product?";
+      default:
+        return "Comment here...";
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="popup">
+        <div className="content">
+          <div className="product">
+            <img
+              style={{ width: 300, height: 300, objectFit: "fill" }}
+              src="http://books.google.com/books/content?id=kLAoswEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api"
+              alt="name"
+            />
+          </div>
+          <div>
+            <h1>{handleText()}</h1>
+            {Array(5)
+              .fill()
+              .map((_, index) =>
+                number >= index + 1 || hoverStar >= index + 1 ? (
+                  <AiFillStar
+                    onMouseOver={() => !number && setHoverStar(index + 1)}
+                    onMouseLeave={() => setHoverStar(undefined)}
+                    style={{ color: "orange" }}
+                    onClick={() => setNumber(index + 1)}
+                  />
+                ) : (
+                  <AiOutlineStar
+                    onMouseOver={() => !number && setHoverStar(index + 1)}
+                    onMouseLeave={() => setHoverStar(undefined)}
+                    style={{ color: "orange" }}
+                    onClick={() => setNumber(index + 1)}
+                  />
+                )
+              )}
+          </div>
+          <textarea placeholder={handlePlaceHolder()}></textarea>
+          <button className={` ${!number && "disabled"} `}>Submit</button>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
